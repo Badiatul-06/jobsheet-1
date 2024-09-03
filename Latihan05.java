b2572861fe47c4b98629280f71bc8ce6ec735964
Badiatul-06 <badiatulmala@gmail.com>
2024-09-03 20:47:58 +0700

    penambahan code program java

---
+++ Latihan05.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan05
+ */
+public class Latihan05 {
+
+    public static void main(String[] args){
+        System.out.print("Nama Saya Badiatul Mala");
+        //System.out.print("\nSaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
